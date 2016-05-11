cb21f551c8aa8f7595687a185fede190df19e2a9
Joel Milne <joel@goldhat.ca>
2016-05-11 17:38:21 +0300

    color template, drafting page and post templates

--- templates/color.php
+++ templates/color.php
@@ -1,3 +1,11 @@
-<div class="acf-field col-md-12">
-  <?php print $view->getFieldValue( $fields ); ?>
+<div class="acf-field acf-field-color-picker col-md-12">
+  <div class="acf-color-display"></div>
 </div>
+
+<style type="text/css">
+.acf-color-display {
+  min-width: 25px;
+  min-height: 25px;
+  background-color: <?php print $view->getFieldValue( $fields ); ?>;
+}
+</style>

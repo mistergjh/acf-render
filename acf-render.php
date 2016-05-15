9b7c53b2dccae0a8b0e8a3316954f60dda8908c0
Joel Milne <joel@goldhat.ca>
2016-05-15 20:46:59 +0300

    removing unused ext constant

--- acf-render.php
+++ acf-render.php
@@ -11,7 +11,6 @@
 define('ACF_RENDER_PLUGIN_DIR', plugin_dir_path( __FILE__ ));
 define('ACF_RENDER_PLUGIN_URL', plugin_dir_url( __FILE__ ));
 define('ACF_RENDER_TEMPLATE_DIR', plugin_dir_path( __FILE__ ) . 'templates/');
-define('ACF_RENDER_TEMPLATE_FILE_EXT', 'php');
 
 class ACFRenderPlugin {
 

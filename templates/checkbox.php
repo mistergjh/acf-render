9d2103197f6ab34280266afa3e029764d686f88f
Joel Milne <joel@goldhat.ca>
2016-05-27 20:45:45 +0300

    improving checkbox template, select template

--- templates/checkbox.php
+++ templates/checkbox.php
@@ -1,1 +1,17 @@
-checkbox template
+<div class="acf-field acf-field-checkbox col-md-12">
+  <div class="acf-checkbox-values">
+    <?php
+      $vals = $template->getFieldValue( $field );
+      if( is_array($vars) && count($vals) >= 1 ) {
+      foreach( $vals as $val ):
+    ?>
+      <div class="acf-checkbox-value">
+        <?php print $val; ?>
+      </div>
+    <?php endforeach; } else { ?>
+      <div class="acf-checkbox-value acf-checkbox-empty">
+        <?php print "No value selected."; ?>
+      </div>
+    <?php } ?>
+  </div>
+</div>
--- templates/select.php
+++ templates/select.php
@@ -1,3 +1,17 @@
-<div class="acf-field col-md-12">
-  <?php print $template->getFieldValue( $field ); ?>
+<div class="acf-field acf-field-checkbox col-md-12">
+  <div class="acf-checkbox-values">
+    <?php
+      $vals = $template->getFieldValue( $field );
+      if( is_array($vars) && count($vals) >= 1 ) {
+      foreach( $vals as $val ):
+    ?>
+      <div class="acf-checkbox-value">
+        <?php print $val; ?>
+      </div>
+    <?php endforeach; } elseif( $vals ) { ?>
+      <div class="acf-checkbox-value acf-checkbox-empty">
+        <?php print $vals; ?>
+      </div>
+    <?php } ?>
+  </div>
 </div>
